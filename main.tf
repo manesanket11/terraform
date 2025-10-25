5711dd792b80e27f4d49cd30ee5e950a2d7820f1
Sanket Mane <60425907+manesanket11@users.noreply.github.com>
2025-10-26 05:23:20 +0800

    Update main.tf
    secret

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      AWS_ACCESS_KEY_ID = AKIA6KJBJD7UPEIACF63
+      AWS_SECRET_ACCESS_KEY = 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
+      AWS_REGION = us-east-1
+
       version = "~> 5.0"
     }
   }
